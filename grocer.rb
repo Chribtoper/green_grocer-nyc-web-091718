0792651f3717a8a57394913abddc96064dd9bb22
Chribtoper <xcstolfax@gmail.com>
2018-09-16 19:11:15 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -40,7 +40,6 @@ end
 def apply_clearance(cart)
   cart.each do |product, attribute|
     if attribute[:clearance] == true
-      math = 0.8
       attribute[:price] = (attribute[:price]*0.8.round(2))
     end
   end
