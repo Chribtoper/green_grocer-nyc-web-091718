a86e6b16506f38448f49abc1f5f4427345553b2c
Chribtoper <xcstolfax@gmail.com>
2018-09-16 19:01:29 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -14,29 +14,26 @@ hash = {}
 end
 
 def apply_coupons(cart:[], coupons:[])
-  coupons.each do |coupon|
-    coupon_name = coupon[:item]
-    coupon_item_num = coupon[:num]
-    cart_item = cart[coupon_name]
-
-    next if cart_item.nil? || cart_item[:count] < coupon_item_num
-
-    cart_item[:count] -= coupon_item_num
-
-    coupon_in_cart = cart["#{coupon_name} W/COUPON"]
-
-    if coupon_in_cart
-      coupon_in_cart[:count] += 1
-    else
-      cart["#{coupon_name} W/COUPON"] = {
-        price: coupon[:cost],
-        clearance: cart_item[:clearance],
-        count: 1
-      }
+    new_hash = {}
+    cart.each do |vegetable, properties|
+      if !new_hash[vegetable]
+        new_hash[vegetable] = properties
+      end
+      coupons.each do |hash|
+        hash.each do |coupon_key, coupon_value|
+          if coupon_value == vegetable
+            if !new_hash["#{vegetable} W/COUPON"]
+              new_hash["#{vegetable} W/COUPON"] = {:price => hash[:cost], :clearance => cart[vegetable][:clearance], :count => 0}
+            end
+            if !(new_hash[vegetable][:count] < hash[:num])
+              new_hash[vegetable][:count] -= hash[:num]
+              new_hash["#{vegetable} W/COUPON"][:count] += 1
+            end
+          end
+        end
+      end
     end
-  end
-
-  cart
+    new_hash
 end
 
 def apply_clearance(cart)
