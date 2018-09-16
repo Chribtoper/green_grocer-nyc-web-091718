2a11303b26571ba6ea7d7f4ef77c7cc7e5c76f14
Chribtoper <xcstolfax@gmail.com>
2018-09-16 19:02:40 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -13,20 +13,31 @@ hash = {}
   hash
 end
 
-def apply_coupons(cart:[], coupons:[])
-  cart.keys.each do |item|
-    coupons.each do |coupon|
-      if item == coupon[:item] && cart[item][:count] >= coupon[:num]
-        cart[item][:count] -= coupon[:num]
-        if cart["#{coupon[:item]} W/COUPON"]
-          cart["#{coupon[:item]} W/COUPON"][:count] += 1
-        else
-          cart["#{coupon[:item]} W/COUPON"] = {:price => coupon[:cost], :clearance => cart[item][:clearance], :count => 1}
-        end
+def apply_coupons(cart, coupons)
+  hash = cart
+  coupons.each do |coupon_hash|
+    # add coupon to cart
+    item = coupon_hash[:item]
+
+    if !hash[item].nil? && hash[item][:count] >= coupon_hash[:num]
+      temp = {"#{item} W/COUPON" => {
+        :price => coupon_hash[:cost],
+        :clearance => hash[item][:clearance],
+        :count => 1
+        }
+      }
+
+      if hash["#{item} W/COUPON"].nil?
+        hash.merge!(temp)
+      else
+        hash["#{item} W/COUPON"][:count] += 1
+        #hash["#{item} W/COUPON"][:price] += coupon_hash[:cost]
       end
+
+      hash[item][:count] -= coupon_hash[:num]
     end
   end
-  cart
+  hash
 end
 
 def apply_clearance(cart)
